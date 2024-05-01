92a063a0729206b09668dbf5c84793382afd3289
김지수 <tttghost@gmail.com>
2024-05-02 02:56:01 +0900

    백트래킹 문제 완료, 마지막 문제 좋았음. 스도쿠

--- 백준/22_백트래킹/test_.py
+++ 백준/22_백트래킹/test_.py
@@ -1,17 +1,60 @@
+#백트래킹을 생각해보자
+
 import os
 
-folderPath=os.path.dirname(os.path.realpath(__file__))
-filePath=folderPath+'/test.txt'
 
-result=[]
-with open(filePath) as file:
-    result=file.readlines()
-print('a')
-line=0
-print(result)
-def readline():
-    global line
-    data=result[line].rstrip()
-    line+=1
-    return data
+def checkRow(row,n):
+    for i in range(N):
+        if board[row][i] == n:
+            return False
+    return True
 
+def checkCol(col,n):
+    for i in range(N):
+        if board[i][col] == n:
+            return False
+    return True
+
+def checkRect(row,col,n):
+    rowStart=row//3*3
+    colStart=col//3*3
+    for i in range(rowStart,rowStart+3):
+        for j in range(colStart,colStart+3):
+            if board[i][j] == n:
+                return False
+    return True
+
+def dfs(n):
+    if n==len(empty_place):
+        print('=================')
+        print('\n'.join(' '.join(map(str,row)) for row in board))
+        for i in board:
+            print(i)
+        exit(0)
+        # return
+
+    row=empty_place[n][0]
+    col=empty_place[n][1]
+    for i in range(1,N+1):
+            if checkRow(row,i) and checkCol(col,i) and checkRect(row,col,i):
+                board[row][col]=i
+                dfs(n+1)
+                board[row][col]=0
+
+
+N=9
+with open( os.path.dirname(os.path.realpath(__file__))+'/6_input.txt') as file:
+    input=file.readline
+    board=[list(map(int, i.split())) for i in file.readlines()]
+
+empty_place=[]
+for i in range(N):
+    for j in range(N):
+        if board[i][j]==0:
+            empty_place.append([i,j])
+print(empty_place)
+
+print('\n'.join(' '.join(map(str,row)) for row in board))
+# print('=================')
+# print('\n'.join(' '.join(map(str,row)) for row in board))
+dfs(0)
