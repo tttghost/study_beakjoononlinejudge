1c92312022f8d0da09a11da1511623683731e8a0
김지수 <tttghost@gmail.com>
2024-05-02 02:58:04 +0900

    파일정리

---
+++ test.py
@@ -0,0 +1,14 @@
+# file=open(0)
+# print(file.readline())
+# print(file)
+# print(file)
+# # v=list(file)
+# # for i in v:
+#     # print(i)
+# # file.write('hihi')
+# file.close()
+
+file=open('test1.txt')
+print(file.readline())
+print(file.readline())
+print(file.readline())
